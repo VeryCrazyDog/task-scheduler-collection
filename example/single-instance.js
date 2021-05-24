7a36c3af21daf10e9479f42ee797366bb71d9e4e
VCD <VeryCrazyDog@gmail.com>
2021-05-24 21:35:33 +0800

    Update with better example

--- example/single-instance.js
+++ example/single-instance.js
@@ -1,24 +1,43 @@
 const { SingleInstanceTaskScheduler, buildEvaluator } = require('../dist')
 
+let apiCallCount = 0
+const startTime = Date.now()
+
 async function fakeApiCall () {
-  console.log('Calling API')
-  const startTime = Date.now()
-  const apiResult = await new Promise(resolve => {
-    setTimeout(resolve, Math.random() * 1000, JSON.stringify({ result: 'SUCCESS' }))
+  console.log(`${Date.now() - startTime}ms: Calling API`)
+  await new Promise(resolve => {
+    setTimeout(() => {
+      apiCallCount++
+      resolve(JSON.stringify({ result: 'SUCCESS' }))
+    }, (apiCallCount + 1) * 400)
   })
-  console.log(`API took ${Date.now() - startTime}ms and returned:`, apiResult)
+  console.log(`${Date.now() - startTime}ms: API returned result`)
 }
 
 const scheduler = new SingleInstanceTaskScheduler(fakeApiCall, {}, {
   nextRunTimeEvaluator: buildEvaluator({
     onSuccess: {
       type: 'RUN_START_TIME',
-      delay: 500
+      delay: 1000
     }
   })
 })
 scheduler.run()
 setTimeout(() => {
-  console.log('Cancelling next run')
+  console.log(`${Date.now() - startTime}ms: Cancelling next run`)
   scheduler.cancelNextRun()
-}, 3 * 1000)
+}, 5 * 1000)
+
+// Sample output:
+// ---------------------------
+// 0ms: Calling API
+// 421ms: API returned result
+// 1005ms: Calling API
+// 1807ms: API returned result
+// 2007ms: Calling API
+// 3209ms: API returned result
+// 3228ms: Calling API
+// 4850ms: API returned result
+// 4851ms: Calling API
+// 5010ms: Cancelling next run
+// 6855ms: API returned result
